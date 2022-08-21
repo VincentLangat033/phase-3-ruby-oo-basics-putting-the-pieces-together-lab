6eb97c1d95dabdebab4f1931dd793bf8be593b8f
Vincent Langat <kimutailangat033@gmail.com>
2022-08-21 12:37:26 +0300

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_accessor :title, :page_count, :author, :genre 
+    attr_reader :title
+    def initialize title
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
 
+    end
+end
+# book = Book.new
+# book.title = "And Then There Were None"
+# book=Book.new("And Then There Were None")
 
